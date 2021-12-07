7009be82449326a4bd200fc634707e1217379ab2
Kaligula1101 <ilya.kalganov.97@mail.ru>
2021-12-07 13:09:50 +0300

    [DELETE] authors api

--- client/src/API/mutations/author.js
+++
@@ -1,9 +0,0 @@
-import {gql} from '@apollo/client'
-
-export const CREATE_AUTHOR = gql`
-    mutation createAuthor($input: AuthorInput) {
-        createAuthor(input: $input) {
-            id, authorName
-        }
-    }
-`
--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import './App.css'
 import {useMutation, useQuery} from "@apollo/client";
-import {GET_ALL_AUTHORS, GET_ONE_AUTHOR} from "./API/query/author";
-import {CREATE_AUTHOR} from "./API/mutations/author";
 import {CREATE_BOOK} from "./API/mutations/book";
 import {GET_ALL_BOOKS, GET_ONE_BOOK} from "./API/query/book";
 
 const App = () => {
-    const {data, loading, error, refetch} = useQuery(GET_ALL_AUTHORS)
-    const {data: oneAuthor, loading: loadingOneAuthor} = useQuery(GET_ONE_AUTHOR, {
-        variables: {
-            id: 1
-        }
-    })
-    const {data: allBooks, loading: loadingAllBooks, error: errorBooks, refetch: refetchBooks} = useQuery(GET_ALL_BOOKS)
+    const {data: allBooks, loading, error, refetch: refetchBooks} = useQuery(GET_ALL_BOOKS)
     const {data:oneBook, loading: loadingOneBook} = useQuery(GET_ONE_BOOK, {
         variables: {
             id: 1
         }
     })
 
-    const [newAuthor] = useMutation(CREATE_AUTHOR)
     const [newBook] = useMutation(CREATE_BOOK)
 
-    const [authors, setAuthors] = useState([])
     const [authorName, setAuthorName] = useState('')
 
     const [books, setBooks] = useState([])
     const [bookName, setBookName] = useState('')
     const [genre, setGenre] = useState('')
 
-    console.log(oneAuthor)
-
-    // useEffect(() => {
-    //     if (!loading) {
-    //         setAuthors(data.getAllAuthors)
-    //     }
-    // }, [data])
 
     useEffect(() => {
         if (!loading) {
             setBooks(allBooks.getAllBooks)
         }
     }, [allBooks])
-
-    // const addAuthor = (e) => {
-    //     e.preventDefault()
-    //     newAuthor({
-    //         variables: {
-    //             input: {
-    //                 authorName
-    //             }
-    //         }
-    //     }).then(({data}) => {
-    //         console.log(data)
-    //         setAuthorName('')
-    //     })
-    // }
 
     const addBook = (e) => {
         e.preventDefault()
@@ -74,11 +43,6 @@ const App = () => {
         })
     }
 
-    const getAllAuthors = e => {
-        e.preventDefault()
-        refetch()
-    }
-
     const getAllBooks = e => {
         e.preventDefault()
         refetchBooks()
@@ -91,11 +55,11 @@ const App = () => {
     return (
         <div className="App">
             <header className="App-header">
-            <form onSubmit="return onSubmit()">
+            <form>
                 <input value={bookName} onChange={e => setBookName(e.target.value)} type="text" placeholder="bookName"/>
                 <input value={genre} onChange={e => setGenre(e.target.value)} type="text" placeholder="genre"/>
                 <input value={authorName} onChange={e => setAuthorName(e.target.value)} type="text" placeholder="authorName"/>
-                <div className="btns">
+                <div>
                     <button onClick={(e) => addBook(e) }>Add</button>
                     <button onClick={e => getAllBooks(e)}>Get</button>
                 </div>
